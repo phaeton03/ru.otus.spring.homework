b3f4d8651f3efbf05d96bdee5a1c0d4985c15438
Nickolay <phaeton03@gmail.com>
2022-01-09 18:32:24 +0300

    Revert "Homework 26 spring batch (#13)"
    This reverts commit a37638d135c058eeca1ab48af8c70880b29e3594.

--- homework_26_SpringBatch/src/main/java/ru/otus/spring/homework_26/repository/jpa/CommentJPARepository.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.spring.homework_26.repository.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.otus.spring.homework_26.domain.h2.CommentJPA;
-
-
-public interface CommentJPARepository extends JpaRepository<CommentJPA, Long> {
-}
--- homework_26_SpringBatch/src/main/java/ru/otus/spring/homework_26/service/AuthorService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.spring.homework_26.service;
-
-public interface AuthorService {
-    void rename(String authorId, String newAuthorName);
-    String getAuthor(String authorId);
-    void writeBiography(String authorId, String history);
-}
